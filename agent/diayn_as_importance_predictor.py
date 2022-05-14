ffa5bf839273bfa59dd287fefec2bfd2c87147ad
jsrimr <jsrimr@naver.com>
2022-05-14 12:43:02 +0000

    implemented simple weight on skill strategy

--- agent/diayn_as_importance_predictor.py
+++ agent/diayn_as_importance_predictor.py
@@ -6,9 +6,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from zmq import device
 import utils
 from dm_env import specs
-from agent.ddpg import Actor, DDPGAgent
+from agent.ddpg import DDPGAgent
 
 
 class DIAYN(nn.Module):
@@ -26,18 +27,8 @@ class DIAYN(nn.Module):
         skill_pred = self.skill_pred_net(obs)
         return skill_pred
 
-class SkillMixingActor(Actor):
-    def __init__(self, obs_type, obs_dim, action_dim, feature_dim, hidden_dim, pretrained_actor, diayn):
-        super().__init__(obs_type, obs_dim, action_dim, feature_dim, hidden_dim)
 
-        self.pretrained_actor = pretrained_actor
-        self.diayn = diayn
 
-    def forward(self, obs, std):
-
-        return super().forward(obs, std)
-
-    
 class DIAYNasWeightPredictorAgent(DDPGAgent):
     def __init__(self, update_skill_every_step, skill_dim, diayn_scale,
                  update_encoder, **kwargs):
@@ -50,77 +41,95 @@ class DIAYNasWeightPredictorAgent(DDPGAgent):
 
         # create actor and critic
         super().__init__(**kwargs)
-        self.diayn = DIAYN(self.obs_dim - self.skill_dim, self.skill_dim,
-                           kwargs['hidden_dim']).to(kwargs['device'])
-        self.diayn.train()
+        # self.diayn = DIAYN(self.obs_dim - self.skill_dim, self.skill_dim,
+        #                    kwargs['hidden_dim']).to(kwargs['device'])
+        # self.diayn.train()
 
-        self.pretrained_actor = self.actor
-        self.actor = SkillMixingActor(self.obs_type, self.obs_dim, self.action_dim,
-                           self.feature_dim, self.hidden_dim, self.pretrained_actor, self.diayn).to(kwargs['device'])
-
-        self.actor_opt = torch.optim.Adam(self.actor.parameters()) + list(self.diayn.parameters()), lr=self.lr)
+        self.weight_param = nn.Parameter(torch.rand(self.skill_dim, device=kwargs['device']))
+        self.actor_opt = torch.optim.Adam(list(self.actor.parameters()) + [self.weight_param], lr=self.lr)
     
 
-    def update_actor(self, obs, step):
+    def update(self, replay_iter, step):
         metrics = dict()
 
-        obs = torch.as_tensor(obs, device=self.device)  # (B, obs_dim)
-        skill_weight = F.softmax(self.diayn(obs), dim=-1).unsqueeze(-1)  # (B, skill_dim, 1)
+        if step % self.update_every_steps != 0:
+            return metrics
 
-        obs = obs.unsqueeze(1).repeat(1, self.skill_dim, 1)  # (B, skill_dim, obs_dim)
-        skill_list = torch.eye(self.skill_dim, device=self.device).unsqueeze(0)  # (1, skill_dim, skill_dim)
-        skill_list = skill_list.repeat(obs.shape[0], 1, 1)  # (B, skill_dim, skill_dim)
-        obs_with_skill = torch.cat([obs, skill_list], dim=-1)  # (B, skill_dim, skill_dim + obs_dim)
+        batch = next(replay_iter)
 
-        inpt = obs_with_skill * skill_weight  # (B, skill_dim, skill_dim + obs_dim)
-        h = self.encoder(inpt)
+        obs, action, extr_reward, discount, next_obs = utils.to_torch(
+            batch, self.device)
 
-        stddev = utils.schedule(self.stddev_schedule, step)
-        dist = self.actor(h, stddev)
-        action = dist.sample(clip=self.stddev_clip)
-        log_prob = dist.log_prob(action).sum(-1, keepdim=True)
-        Q1, Q2 = self.critic(obs, action)
-        Q = torch.min(Q1, Q2)
+        # augment and encode : state 일 땐 aug_and_encode 의미없음
+        obs = self.aug_and_encode(obs)
+        next_obs = self.aug_and_encode(next_obs)
 
-        actor_loss = -Q.mean()
-
-        # optimize actor
-        self.actor_opt.zero_grad(set_to_none=True)
-        actor_loss.backward()
-        # actor_loss.backward(retain_graph=True)
-        self.actor_opt.step()
+        reward = extr_reward
 
         if self.use_tb or self.use_wandb:
-            metrics['actor_loss'] = actor_loss.item()
-            metrics['actor_logprob'] = log_prob.mean().item()
-            metrics['actor_ent'] = dist.entropy().sum(dim=-1).mean().item()
+            metrics['extr_reward'] = extr_reward.mean().item()
+            metrics['batch_reward'] = reward.mean().item()
+
+        if not self.update_encoder:
+            obs = obs.detach()
+            next_obs = next_obs.detach()
+
+        # extend observations with skill
+        # obs = torch.cat([obs, skill], dim=1)
+        # next_obs = torch.cat([next_obs, skill], dim=1)
+
+        obs = self.mix_skill_obs(obs)
+        next_obs = self.mix_skill_obs(next_obs)
+
+        # update critic
+        metrics.update(
+            self.update_critic(obs.detach(), action, reward, discount,
+                               next_obs.detach(), step))
+
+        # update actor
+        metrics.update(self.update_actor(obs.detach(), step))
+
+        # update critic target
+        utils.soft_update_params(self.critic, self.critic_target,
+                                 self.critic_target_tau)
 
         return metrics
+
+    def mix_skill_obs(self, obs):
+        """
+        (B, obs_dim) => (B, skill_dim + obs_dim)
+        """
+        obs = torch.as_tensor(obs, device=self.device).unsqueeze(1)  # (B, 1, obs_dim)
+        obs = obs.repeat(1, self.skill_dim, 1)  # (B, skill_dim, obs_dim)
+        
+        skill_list = torch.eye(self.skill_dim, device=self.device).unsqueeze(0)  # (1, skill_dim, skill_dim)
+        skill_list = skill_list.repeat(obs.shape[0], 1, 1)  # (B, skill_dim, skill_dim)
+        state_with_skill = torch.cat([obs, skill_list], dim=-1)  # (B, skill_dim, skill_dim + obs_dim)
+
+        # skill_weight = F.softmax(self.diayn(obs), dim=-1).unsqueeze(-1)  # (B, skill_dim, 1)
+        skill_weight = F.softmax(self.weight_param, dim=0).unsqueeze(0).repeat(obs.shape[0],1).unsqueeze(-1)  # (B, skill_dim, 1)
+
+        processed = state_with_skill * skill_weight  # (B, skill_dim, skill_dim + obs_dim)
+
+        return processed.sum(dim=1)  # (B, skill_dim + obs_dim)
 
     def act(self, obs, meta, step, eval_mode):
         """
         meta from passed parameter is useless
         """
         #assert obs.shape[-1] == self.obs_shape[-1]
-        with torch.no_grad():
-            obs = torch.as_tensor(obs, device=self.device).unsqueeze(0) # (1, obs_dim)
-            skill_weight = F.softmax(self.diayn(obs), dim=-1)  # (1, skill_dim)
-
-            obs = obs.repeat(self.skill_dim, 1)  # (skill_dim, obs_dim)
-            skill_list = torch.eye(self.skill_dim, device=self.device) # (skill_dim, skill_dim)
-            obs_with_skill = torch.cat([obs, skill_list], dim=-1)  # (skill_dim, skill_dim + obs_dim)
-
-            inpt = obs_with_skill * skill_weight.view(self.skill_dim, 1)  # (skill_dim, skill_dim + obs_dim)
-
-            h = self.encoder(inpt)
-            stddev = utils.schedule(self.stddev_schedule, step)
-            dist = self.actor(h, stddev)
-            if eval_mode:
-                action = dist.mean
-            else:
-                action = dist.sample(clip=None)
-                if step < self.num_expl_steps:
-                    action.uniform_(-1.0, 1.0)
+        obs = torch.as_tensor(obs, device=self.device).unsqueeze(0) # (1, obs_dim)
+        
+        inpt = self.mix_skill_obs(obs)
+        h = self.encoder(inpt)
+        stddev = utils.schedule(self.stddev_schedule, step)
+        dist = self.actor(h, stddev)
+        if eval_mode:
+            action = dist.mean
+        else:
+            action = dist.sample(clip=None)
+            if step < self.num_expl_steps:
+                action.uniform_(-1.0, 1.0)
 
         return action.cpu().numpy()[0]
         
@@ -128,7 +137,7 @@ class DIAYNasWeightPredictorAgent(DDPGAgent):
     def init_from(self, other):
         # copy parameters over
         utils.hard_update_params(other.encoder, self.encoder)
-        utils.hard_update_params(other.actor, self.pretrained_actor)
-        utils.hard_update_params(other.diayn, self.diayn)
+        utils.hard_update_params(other.actor, self.actor)
+        # utils.hard_update_params(other.diayn, self.diayn)
         if self.init_critic:
             utils.hard_update_params(other.critic.trunk, self.critic.trunk)
